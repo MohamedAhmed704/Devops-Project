d86f3d34912a4db12de8a1bb74e03c6aa48a2720
Hossam Aldeen <hossamaldeenabdalah@gmail.com>
2025-12-04 00:36:21 +0200

    fixed bugs and return orderId

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from "axios";
 
 const PAYMOB_API_KEY = process.env.PAYMOB_API_KEY;
 const PAYMOB_API_URL = process.env.PAYMOB_API_URL;
@@ -11,7 +11,6 @@ async function getAuthToken() {
   });
   return res.data.token;
 }
-
 async function createOrder(token, amount) {
   const res = await axios.post(`${PAYMOB_API_URL}/ecommerce/orders`, {
     auth_token: token,
@@ -20,10 +19,8 @@ async function createOrder(token, amount) {
     currency: "EGP",
     items: [],
   });
-
   return res.data.id;
 }
-
 async function createPaymentKey(token, orderId, amount) {
   const res = await axios.post(`${PAYMOB_API_URL}/acceptance/payment_keys`, {
     auth_token: token,
@@ -45,27 +42,44 @@ async function createPaymentKey(token, orderId, amount) {
     currency: "EGP",
     integration_id: PAYMOB_INTEGRATION_ID,
   });
-
   return res.data.token;
 }
-
 export async function createPayment(req, res) {
   try {
     const { amount } = req.body;
-
     const token = await getAuthToken();
     const orderId = await createOrder(token, amount);
     const paymentKey = await createPaymentKey(token, orderId, amount);
 
     const iframeURL = `https://accept.paymob.com/api/acceptance/iframes/${PAYMOB_API_IFRAME}?payment_token=${paymentKey}`;
 
-    res.json({
-      success: true,
-      iframeURL,
-    });
+    res.json({ success: true, iframeURL, orderId });
   } catch (err) {
     console.error("Error creating payment:", err.message);
     res.status(500).json({ success: false, error: err.message });
   }
 }
 
+export async function userPaid(req, res) {
+  const { orderId } = req.params;
+  try {
+    const token = await getAuthToken();
+    const result = await axios.get(
+      `${PAYMOB_API_URL}/ecommerce/orders/${orderId}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    console.log(result.data);
+    console.log("--------------------");
+    console.log(result.data.is_paid);
+
+    if (result.data && result.data.payment_status === "PAID") {
+      res.json({ success: true });
+    } else {
+      res.json({ success: false });
+    }
+  } catch (err) {
+    res.json({ success: false, error: err.message });
+  }
+}
--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPayment } from "../controllers/paymentController.js";
+import { createPayment, userPaid } from "../controllers/paymentController.js";
 
 
 const router = express.Router();
 
-
+// here the useris requestss to payy
 router.post('/pay',createPayment)
-
+// here we are checking if the users paid or not !
+router.get("/paymentStatus/:orderId", userPaid);
 export default router
