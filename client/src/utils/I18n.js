65110284fe2cfacc7761709a7d2ec360381896cf
Ahmedl-Be <ahmedabdallabeltagy@gmail.com>
2025-12-08 09:44:07 +0200

    set default lang to en

--- client/src/utils/I18n.js
+++ client/src/utils/I18n.js
@@ -12,7 +12,7 @@ import arTranslations from "../locales/ar/translation.json";
       translation: arTranslations,  
     },
   },
-  lng: "ar",
+  lng: "en",
   fallbackLng: "ar",
   interpolation: {
     escapeValue: false,
