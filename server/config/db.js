b6b7d97c54ca3c025cc8365f5ff82dcd476df6c6
Ubuntu <ubuntu@ip-172-31-8-203.ec2.internal>
2026-05-14 12:43:31 +0000

    Deploy project to create CI-CD & Create EKS Cluster in AWS

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+      maxPoolSize: 10,
+    });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(`Error: ${err.message}`);
-  // This forces the app to stop if the connection fails.
-  // 1 means the process exited due to an error.
-  // It’s safer to stop here than let your server run without a database connection.
-    process.exit(1);
+    console.error(`MongoDB Connection Error: ${err.message}`);
+
+    // بدل ما يموت فورًا ندي retry
+    setTimeout(connectDB, 5000);
   }
 };
 
